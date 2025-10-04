87661ba054f3febc623c8f9f81ce5c00af570595
Romain <romain.gilliot@motion-live.com>
2025-10-04 22:46:33 +0200

    first fxhub event in fxommander and the possibility to start it with the argument -do-nothing so the app don't exec the process but still fires the events.

---
+++ src/Commander_gen.h
@@ -0,0 +1,6 @@
+//This is a generated file, don't change it manually, it will be override when rebuild.
+
+bool doNothing(){return _doNothing;}
+const bool doNothing() const {return _doNothing;}
+void setDoNothing(bool doNothing){_doNothing = doNothing;}
+
